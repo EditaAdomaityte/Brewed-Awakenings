0cf3a8cd6384c9fb067d32e020b39e8d130f4e99
Edita Adomaityte <adomaityteedita02@gmail.com>
2024-12-04 16:49:31 -0500

    final commit

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,4 +1,4 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
 
@@ -6,7 +6,9 @@ export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li data-id="${employee.id}"
+                     data-type="emp"
+                >${employee.name}</li>`
     }
 
     html += "</ul>"
@@ -14,3 +16,25 @@ export const Employees = () => {
     return html
 }
 
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const employeeClinked=clickEvent.target
+        if (employeeClinked.dataset.type === "emp" ){
+            const employeeId=parseInt(employeeClinked.dataset.id)
+            //const bestEmploy = []
+            const orders=getOrders()
+            let prodSold=0
+            for (const order of orders){
+
+                if (employeeId === order.employeeId){
+                prodSold++
+                }
+          }window.alert(`${employeeClinked.innerText} sold ${prodSold} products`)
+        }
+
+
+
+    }
+)
--- scripts/Products.js
+++ scripts/Products.js
@@ -6,11 +6,22 @@ export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li data-type="product"
+                     data-cost="${product.price}">${product.name}</li>`
     }
 
     html += "</ul>"
 
     return html
 }
+ document.addEventListener(
+    "click",
+    (clickEvent)=>{
+        const prodClicked=clickEvent.target
+        const cost=prodClicked.dataset.cost
+        if (prodClicked.dataset.type === "product")
+            window.alert(`${prodClicked.innerText} costs \n${cost}$`)
 
+    }
+
+ )
